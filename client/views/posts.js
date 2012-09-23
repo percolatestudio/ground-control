77dac6c7e212449a9271066c3fa865535b7dfc2a
Tom Coleman <tom@thesnail.org>
2012-09-23 15:28:12 +1000

    Formatted dates (a touch).

--- client/views/posts.js
+++ client/views/posts.js
@@ -1,3 +1,11 @@
 Template.posts.helpers({
   posts: function() { return Posts.find(); }
 });
+
+Template.post.helpers({
+  // XXX: this should probably be global
+  formatDate: function(date) {
+    // XXX: format... use moment.js?
+    return new Date(date);
+  }
+})
