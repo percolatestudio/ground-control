aec08a23da004825f69e99a1f4e9e2a0ba603537
Tom Coleman <tom@thesnail.org>
2012-09-23 17:24:52 +1000

    Super simple edit interface

--- client/views/posts.js
+++ client/views/posts.js
@@ -3,9 +3,41 @@ Template.posts.helpers({
 });
 
 Template.post.helpers({
+  'editing': function() { 
+    // XXX: how can we avoid using a session var for this kind of thing?
+    // can we some how use the template instance reactively?
+    return Session.equals('editingPostId', this._id);
+  }
+})
+
+Template.post.events({
+  'click .edit': function(e, instance) { 
+    Session.set('editingPostId', this._id);
+  }
+});
+
+Template.showPost.helpers({
   // XXX: this should probably be global
   formatDate: function(date) {
     // XXX: format... use moment.js?
     return new Date(date);
+  }
+});
+
+Template.editPost.events({
+  'click .cancel': function() {
+    Session.set('editingPostId', null);
+  },
+  
+  'submit form': function(e, template) {
+    e.preventDefault();
+    
+    var change = {}
+    change.title = template.find('[name=title]').value;
+    change.author = template.find('[name=author]').value;
+    change.story = template.find('[name=story]').value;
+    
+    Posts.update(this._id, {$set: change});
+    Session.set('editingPostId', null);
   }
 })
--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,10 @@
 Meteor.publish('posts', function() {
   return Posts.find();
+});
+
+// XXX: do auth properly
+Posts.allow({
+  'update': function() { 
+    return true;
+  }
 })
