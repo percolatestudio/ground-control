543bde31ab85e9eb6bb8b2a5733cc026da0d1b07
Tom Coleman <tom@thesnail.org>
2012-09-28 12:09:02 +1000

    Restrict signup based on email.
    Doesn't actually show the user anything if they don't match unfortunately.

--- server/publish.js
+++ server/publish.js
@@ -15,3 +15,17 @@ Meteor.methods({
     return Meteor.users.find().count() === 0;
   }
 })
+
+// user account validation. Set this value in server/configuration.js
+Meteor.accounts.validateNewUser(function(proposedUser) {
+  var email = proposedUser.services.google.email;
+  
+  function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  }
+  
+  if (_.isRegExp(Meteor.accounts.allowedEmails))
+    return Meteor.accounts.allowedEmails.test(email);
+  else
+    return endsWith(email, Meteor.accounts.allowedEmails);
+});
