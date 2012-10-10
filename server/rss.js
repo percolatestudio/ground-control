900c6671f72a6a50f667d556ff8accd848c11d32
Tom Coleman <tom@thesnail.org>
2012-10-10 11:14:59 +1100

    Successfully packaged up node-xml and included it.

--- server/rss.js
+++ server/rss.js
@@ -1,23 +1,4 @@
 // serve up RSS at the right url
 Meteor.serve('feed.xml', function() {
-  var RSS = __meteor_bootstrap__.require("rss")
-  
-  var feed = new RSS({
-    title: Settings.title,
-    description: Settings.description
-  });
-  
-  var converter = new Showdown.converter();
-  Posts.find().forEach(function(post) {
-    feed.item({
-     title: post.title,
-     description: converter.makeHtml(post.body),
-     author: post.author,
-     date: post.publishedAt,
-     // XXX: work this out properly
-     url: '/posts/' + post.slug
-    });
-  });
-  
-  return feed.xml();
+  return XML([ { url: 'http://www.google.com/search?aq=f&sourceid=chrome&ie=UTF-8&q=opower' } ]);
 });
