f99d833a2a4961effb6e7acb7df66b8c3872a221
Tom Coleman <tom@thesnail.org>
2012-10-18 17:58:44 +1100

    Expand the extra posts a little.

--- server/fixtures.js
+++ server/fixtures.js
@@ -45,7 +45,13 @@ Meteor.startup(function() {
         author: 'Matt DeBergalis',
         authorGravatarHash: '182ca8f6769f9e589ccfe4c15b9a130a',
         publishedAt: new Date(2012, 7, i),
-        body: "Simple body\n\nThat's it."
+        body: [
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+          "",
+          "     for (i = 0; i < 1000; i++) {",
+          "       console.log('foo');",
+          "     }"
+          ].join('\n\n')
       }); 
     }
   }
