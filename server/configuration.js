55e921457e1f2aed6e21a3f880a842f5b21461fe
Tom Coleman <tom@thesnail.org>
2012-11-25 23:39:43 +1100

    Preparing the deployment for percolate.

--- server/configuration.js
+++ server/configuration.js
@@ -1,7 +1,7 @@
 _.extend(GroundControlConfig, {
   // Set the email domain (or regexp) that signing up users must conform to
-  allowedEmails: /(@meteor\.com|@percolatestudio\.com)$/,
+  allowedEmails: /(@percolatestudio\.com)$/,
   
   // load up some test data from meteor.com/blog when the page first loads
-  loadFixtures: true
+  // loadFixtures: true
 });
