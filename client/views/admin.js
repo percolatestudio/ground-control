929f919b2a447b0fcf0aafe280eaff99b0fd78ec
Tom Coleman <tom@thesnail.org>
2012-12-18 12:53:10 +1100

    You can now update the email domain setting.

--- client/views/admin.js
+++ client/views/admin.js
@@ -4,6 +4,20 @@ Template.admin.helpers({
   }
 });
 
+Template.adminSettings.helpers({
+  emailDomain: function() { return getSetting('emailDomain'); }
+})
+
+Template.adminSettings.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+    
+    var domain = template.find('[name=emailDomain]').value;
+    // XXX: validation?
+    updateSetting('emailDomain', domain);
+  }
+})
+
 Template.adminPostsList.helpers({
   posts: function() {
     return allPosts();
